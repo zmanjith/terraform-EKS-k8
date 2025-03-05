47d8cb21d9aac6ecfffd8ccca3a7d0d1c09ee160
MANJITH MADHUSUDANAN <42224522+zmanjith@users.noreply.github.com>
2025-03-05 16:08:12 -0600

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -11,7 +11,7 @@ module "vpc" {
   name = "tr-eks-k8-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-1", "us-east-2"]
+  azs             = data.aws_availability_zones.available.names
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
