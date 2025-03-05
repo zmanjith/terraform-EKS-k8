76154314b66924be355db35ac79885becd806566
MANJITH MADHUSUDANAN <42224522+zmanjith@users.noreply.github.com>
2025-03-05 16:05:55 -0600

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -11,7 +11,7 @@ module "vpc" {
   name = "tr-eks-k8-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-west-1a", "eu-west-1b"]
+  azs             = ["us-east-1", "us-east-2"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
