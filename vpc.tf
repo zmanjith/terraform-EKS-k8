76fae1720b66f5f970b1c784ee9e7c91b66966aa
zmanjith <42224522+zmanjith@users.noreply.github.com>
2025-03-05 12:43:04 -0600

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,1 +1,20 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.19.0"
 
+  name = "tr-eks-k8-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+
+}
